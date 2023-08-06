1d4051e58ee70659424cfefc315f9d8eef1cb6aa
mingmoth <mingmoth@gmail.com>
2023-08-06 20:38:22 +0800

    feat: add Todo component

---
+++ src/components/todo/Todo.jsx
@@ -0,0 +1,10 @@
+export default function Todo ({ todo }) {
+    return (
+        <>
+            <label>
+                <input></input>
+                { todo.text}
+            </label>
+        </>
+    )
+}
